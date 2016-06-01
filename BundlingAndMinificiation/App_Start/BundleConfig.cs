0fe90b2119e19a8da9e3f33032378daf4f7e9b28
vib-hu <vibhumishra808@gmail.com>
2016-06-01 23:42:22 +0530

    first

---
+++ BundlingAndMinificiation/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace BundlingAnndMinificiation.App_Start
+{
+    public class BundleConfig
+    {
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new ScriptBundle("~/bundle/jquery").
+                Include("~/Scripts/jquery-{version}.js").
+                Include("~/Scripts/bootstrap.js"));
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ BundlingAndMinificiation/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using BundlingAnndMinificiation.App_Start;
+using System.Web.Optimization;
+
+namespace BundlingAndMinificiation
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
